174e8372c8b33cee492e6ba8cb435534edfadfab
Unknown <romain.romss@gmail.com>
2017-12-27 17:21:18 +0100

    New Controllers is ok

--- src/Controllers/ArticleDetailsController.php
+++ src/Controllers/ArticleDetailsController.php
@@ -8,30 +8,30 @@ use Romss\Models\PostsTable;
 class ArticleDetailsController extends  Controller
 {
     /**
+     * @param int $id
      * @return string
      * @throws \Twig_Error_Loader
      * @throws \Twig_Error_Runtime
      * @throws \Twig_Error_Syntax
      */
-    private function getArticle() {
+    private function getArticle(int $id) {
         $posts = new PostsTable($this->db);
-        $posts->getPost('id');
-        var_dump($posts);
+        $post = $posts->getPost($id)->fetch();
 
-        return  $this->render('Article/postdetails', ['id' => $posts]);
+        return  $this->render('Article/postdetails', ['post' => $post]);
     }
 
     /**
-     * @param array $param
+     * @param array $params
      * @return string
      * @throws \Twig_Error_Loader
      * @throws \Twig_Error_Runtime
      * @throws \Twig_Error_Syntax
      */
-    function __invoke(array $param)
+    function __invoke(array $params)
     {
         if ($_SERVER['REQUEST_METHOD'] === 'GET'){
-            return $this->getArticle();
+            return $this->getArticle($params['id'] ?? 0);
         }
         return  'Not found';
     }
