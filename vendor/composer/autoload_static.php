2eb5dbabf53e22f89341c33c629a82937ba47bc4
Unknown <romain.romss@gmail.com>
2017-12-27 17:26:26 +0100

    Same files

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -13,6 +13,7 @@ class ComposerStaticInit5d2c435062326fdb59df3eced3b08226
     public static $prefixLengthsPsr4 = array (
         'T' => 
         array (
+            'Twig\\Extensions\\' => 16,
             'Twig\\' => 5,
         ),
         'S' => 
@@ -26,6 +27,10 @@ class ComposerStaticInit5d2c435062326fdb59df3eced3b08226
     );
 
     public static $prefixDirsPsr4 = array (
+        'Twig\\Extensions\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/twig/extensions/src',
+        ),
         'Twig\\' => 
         array (
             0 => __DIR__ . '/..' . '/twig/twig/src',
@@ -43,6 +48,10 @@ class ComposerStaticInit5d2c435062326fdb59df3eced3b08226
     public static $prefixesPsr0 = array (
         'T' => 
         array (
+            'Twig_Extensions_' => 
+            array (
+                0 => __DIR__ . '/..' . '/twig/extensions/lib',
+            ),
             'Twig_' => 
             array (
                 0 => __DIR__ . '/..' . '/twig/twig/lib',
