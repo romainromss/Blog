0257e9f745c7a4dee8ab97e932b97a37c0b1879b
Unknown <romain.romss@gmail.com>
2017-12-28 19:05:33 +0100

    Activate debug twig extension

--- src/App.php
+++ src/App.php
@@ -2,6 +2,7 @@
 
 namespace Romss;
 
+use Twig\Extensions\TextExtension;
 use Twig_Environment;
 use Twig_Extensions_Extension_Text;
 use Twig_Loader_Filesystem;
@@ -19,8 +20,10 @@ class App
         $loader = new Twig_Loader_Filesystem(__DIR__ . '/../Views');
         $this->twig = new Twig_Environment($loader, [
             'cache' => false,
+            'debug' => true,
         ]);
         $this->twig->addExtension(new Twig_Extensions_Extension_Text());
+        $this->twig->addExtension(new \Twig_Extension_Debug());
 
         $this->db = new Database('mysql:host=127.0.0.1;dbname=blog', 'root', '23031991');
     }
