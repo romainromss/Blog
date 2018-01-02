1b5f2d0b8cd72bb464c4d04b650f736d38126e58
Unknown <romain.romss@gmail.com>
2018-01-02 19:05:05 +0100

    Add method setFlash() and setErrors(), Add $param: session and page for twig

--- src/Controllers/Controller.php
+++ src/Controllers/Controller.php
@@ -31,10 +31,36 @@ class Controller
         exit();
     }
 
-    public function getInput($input): string
+    public function getInput($input): ?string
     {
         if (isset($_POST[$input])) return $input;
         return null;
+    }
+
+    public function setFlash($type, $content)
+    {
+        $_SESSION['flash'] = compact('type', 'content');
+    }
+
+    /**
+     * Permet d'enregistrer les erreurs et le formulaire
+     *
+     * @param array $errors
+     * @param array $post
+     */
+    public function setErrors($errors, $post)
+    {
+        // On enregistre notre formulaire
+        $_SESSION['post'] = $post;
+
+        $content = '<span>Des erreurs sont survenues:</span><ul>';
+        foreach ($errors as $error) {
+            $content .= '<li>'.$error.'</li>';
+        }
+        $content .= '</ul>';
+
+        // On enregistre le contenu de l'erreur
+        $this->setFlash('danger', $content);
     }
 
     /**
@@ -47,6 +73,14 @@ class Controller
      */
     protected function render($file, $params = [])
     {
+        if (isset($_SESSION['flash'])) {
+            $params['__flash'] = $_SESSION['flash'];
+            unset($_SESSION['flash']);
+        }
+
+        $params['__session'] = $_SESSION;
+        $params['__page'] = $file.'.html.twig';
+
         return $this->twig->render($file.'.html.twig', $params);
     }
 }
