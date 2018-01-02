18b38693fe0755ee41929de118f3dae98b21763c
Unknown <romain.romss@gmail.com>
2018-01-02 19:03:14 +0100

    Add method checkRemember()

--- public/index.php
+++ public/index.php
@@ -8,15 +8,14 @@ session_start();
 require __DIR__ .'/../vendor/autoload.php';
 
 $app = new App();
+$app->checkRemember();
 
 $router = new Router($app);
+
 try {
     $result = $router->handleRequest($_SERVER['REQUEST_URI']);
+    echo $result;
 } catch (Twig_Error_Loader $e) {
 } catch (Twig_Error_Runtime $e) {
 } catch (Twig_Error_Syntax $e) {
 }
-echo $result;
-
-
-
