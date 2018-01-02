17daf95a404eb687cccc3f71c794ac90736ad3a0
Unknown <romain.romss@gmail.com>
2018-01-02 19:08:44 +0100

    Add generateToken

--- src/Controllers/Auth/VerifyAuthentication.php
+++ src/Controllers/Auth/VerifyAuthentication.php
@@ -2,3 +2,19 @@
 
 namespace Romss\Controllers\Auth;
 
+use Romss\Controllers\Controller;
+
+class  VerifyAuthentication extends Controller
+{
+    protected function generateToken($user = null)
+    {
+        if ($user) {
+            $token = $user['id'].'---'.hash('sha512', $user['email'].'#~!*$'.$user['password']);
+        } else {
+            $token = hash('sha512', uniqid().'---'.time());
+            $_SESSION['csrf'] = $token;
+        }
+
+        return $token;
+    }
+}
